710dbfb46c23c37f7d51e09f7972e98863942174
wrnkbl <ul0285074@edu.uni.lodz.pl>
2025-03-15 11:43:54 +0100

    modified:   Project_6/obj/Debug/net9.0/Project_6.AssemblyInfo.cs 	modified:   Project_6/obj/Debug/net9.0/Project_6.AssemblyInfoInputs.cache 	modified:   Project_6/obj/Debug/net9.0/Project_6.csproj.CoreCompileInputs.cache 	modified:   Project_6/obj/Debug/net9.0/Project_6.csproj.FileListAbsolute.txt 	modified:   Project_6/obj/Debug/net9.0/Project_6.dll 	modified:   Project_6/obj/Debug/net9.0/Project_6.pdb 	modified:   Project_6/obj/Debug/net9.0/Project_6.sourcelink.json 	modified:   Project_6/obj/Debug/net9.0/refint/Project_6.dll

--- Project_6/obj/Debug/net9.0/Project_6.AssemblyInfo.cs
+++ Project_6/obj/Debug/net9.0/Project_6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b9f69e4ffdfebe9c8205aa7a16353b4d495d8de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b23e096eaa0939ea6bee18bf68584ab1b8f4419")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
